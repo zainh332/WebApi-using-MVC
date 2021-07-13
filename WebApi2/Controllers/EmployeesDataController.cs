1b42df95436ded35caca2907eb93a405a0cf2f4a
Zain <Zain@DESKTOP-A9BP6R1>
2021-07-14 01:32:50 +0500

    Add project files.

---
+++ WebApi2/Controllers/EmployeesDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        
+        public string[] employeesdata = {"zain", "usama", "adil"};
+
+        public string[] getemployee()
+        {
+            return employeesdata;
+        }
+
+        public string getemployessdata(int id)
+        {
+            return employeesdata [id];
+        }
+    }
+}
